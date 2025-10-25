aff281d060114e5aaf95ff99dee2861b304687f0
Santiago Paredes Suarez <santiparedes2004@gmail.com>
2025-10-24 22:34:35 -0600

    Created swift project

---
+++ 3mosq/3mosq/_mosqApp.swift
@@ -0,0 +1,17 @@
+//
+//  _mosqApp.swift
+//  3mosq
+//
+//  Created by Santiago Paredes on 24/10/25.
+//
+
+import SwiftUI
+
+@main
+struct _mosqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
